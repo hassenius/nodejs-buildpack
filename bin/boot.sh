9a04e9247abf1b4c0f409b8b42270ca0614f6d67
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2016-11-21 13:13:58 +0000

    add more debug info

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,16 +2,24 @@ export APP_ROOT=$HOME
 
 erb $APP_ROOT/config/nginx.conf.orig > $APP_ROOT/config/nginx.conf
 
+echo "Current config"
+cat $APP_ROOT/config/nginx.conf
+
+echo "Home dir"
+dir $APP_ROOT
+
 # ------------------------------------------------------------------------------------------------
 
-#mkfifo $APP_ROOT/nginx/logs/access.log
-#mkfifo $APP_ROOT/nginx/logs/error.log
+mkfifo $APP_ROOT/nginx/logs/access.log
+mkfifo $APP_ROOT/nginx/logs/error.log
 
-#cat < $APP_ROOT/nginx/logs/access.log &
-#(>&2 cat) < $APP_ROOT/nginx/logs/error.log &
+cat < $APP_ROOT/nginx/logs/access.log &
+(>&2 cat) < $APP_ROOT/nginx/logs/error.log &
 
 nginx_bin = $(which nginx)
-$nginx_bin -p $APP_ROOT -c $APP_ROOT/config/nginx.conf &
+echo "Found nginx binary: ${nginx_bin}}"
+
+$APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/config/nginx.conf &
 exec npm start
 
 # ------------------------------------------------------------------------------------------------
